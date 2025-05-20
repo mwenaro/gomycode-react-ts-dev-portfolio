abf85a8615708c1c384169755bb393f8b54b3feb
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-05-20 19:45:29 +0300

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div>
+      {/* Nav */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Projects */}
+
+      {/* Contact */}
+
+      {/* Footer */}
+
+    </div>
+  );
+}
