f288bee45f47d4305cbc7d64599abd127a0fe3b5
rodgers Kipkurui <korirrodgers103@gmail.com>
2025-05-20 21:36:44 +0300

    Added hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 export default function App() {
   return (
@@ -6,7 +7,7 @@ export default function App() {
       {/* Header */}
       <Header />
       {/* Hero */}
-
+      <Hero />
       {/* About */}
 
       {/* My Tech Stack */}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function Hero() {
+  return (
+    <section className="flex items-center justify-center md:p-12">
+      {/* text content */}
+      <div className="flex-1 max-w">
+        <h2 className="text-4xl font-bold mb-2">
+          Hi 👋, <br />
+          My name is <br />
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to purple-600">Pavan MG</span>
+           <br />I build things for web
+        </h2>
+      </div>
+
+      {/* hero image */}
+
+      <div className="flex-1 flex justify-center md:justify-end">
+        <img src="./hero-pic.png" alt="hero iamge" className="w-64 h-64" />
+      </div>
+    </section>
+  );
+}
