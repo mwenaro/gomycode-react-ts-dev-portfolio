82666a4bf078c23ff8f30e3ae55fae99a4d32ed1
Mark Njoroge <njorogemarkwanjiru@gmail.com>
2025-05-22 12:04:27 +0300

    add project section

---
+++ src/components/ProjectSection.tsx
@@ -0,0 +1,151 @@
+
+export function ProjectSection() {
+    return (
+        <section className="px-18 py-2 max-w-[1600px]">
+            <h2 className="font-bold text-4xl text-[#42446E]">Projects</h2>
+            <p className="my-4">Things I've built so far</p>
+
+            <div className=" flex flex-wrap">
+
+                {/* project 1 */}
+                <div className="max-w-[250px] pb-2 m-4 bg-white rounded-xl shadow">
+                    {/* image */}
+                    <img src="/images/project-1.png" alt="My first project" />
+
+                    <div className="m-3">
+                        <h3 className="font-medium text-center">Project Tile goes here</h3>
+                        {/* text content */}
+                        <p className="font-light">This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</p>
+
+                        <span className="text-sm m-3 inline-block">Tech stack : HTML , JavaScript, SASS, React</span>
+
+                        <div className="inline-flex flex gap-2">
+                            {/* icons */}
+                            <img src="./live-preview-icon.png" alt="live preview icon" />
+                            <span className="text-sm underline">Live Preview</span>
+
+                            <img src="./github-icon.png" alt="githud icon" />
+                            <span className="text-sm underline">View Code</span>
+                        </div>
+                    </div>
+                </div>
+
+                {/* project 2 */}
+                <div className="max-w-[250px] pb-2 m-4 bg-white rounded-xl shadow">
+                    {/* image */}
+                    <img src="/images/project-2.png" alt="My second project" />
+
+                    <div className="m-3">
+                        <h3 className="font-medium text-center">Project Tile goes here</h3>
+                        {/* text content */}
+                        <p className="font-light">This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</p>
+
+                        <span className="text-sm m-3 inline-block">Tech stack : HTML , JavaScript, SASS, React</span>
+
+                        <div className="inline-flex flex gap-2">
+                            {/* icons */}
+                            <img src="./live-preview-icon.png" alt="live preview icon" />
+                            <span className="text-sm underline">Live Preview</span>
+
+                            <img src="./github-icon.png" alt="githud icon" />
+                            <span className="text-sm underline">View Code</span>
+                        </div>
+                    </div>
+                </div>
+
+                {/* project 3 */}
+                <div className="max-w-[250px] pb-2 m-4 bg-white rounded-xl shadow">
+                    {/* image */}
+                    <img src="/images/project-3.png" alt="My third project" />
+
+                    <div className="m-3">
+                        <h3 className="font-medium text-center">Project Tile goes here</h3>
+                        {/* text content */}
+                        <p className="font-light">This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</p>
+
+                        <span className="text-sm m-3 inline-block">Tech stack : HTML , JavaScript, SASS, React</span>
+
+                        <div className="inline-flex flex gap-2">
+                            {/* icons */}
+                            <img src="./live-preview-icon.png" alt="live preview icon" />
+                            <span className="text-sm underline">Live Preview</span>
+
+                            <img src="./github-icon.png" alt="githud icon" />
+                            <span className="text-sm underline">View Code</span>
+                        </div>
+                    </div>
+                </div>
+
+                {/* project 4 */}
+                <div className="max-w-[250px] pb-2 m-4 bg-white rounded-xl shadow">
+                    {/* image */}
+                    <img src="/images/project-4.png" alt="My fourth project" />
+
+                    <div className="m-3">
+                        <h3 className="font-medium text-center">Project Tile goes here</h3>
+                        {/* text content */}
+                        <p className="font-light">This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</p>
+
+                        <span className="text-sm m-3 inline-block">Tech stack : HTML , JavaScript, SASS, React</span>
+
+                        <div className="inline-flex flex gap-2">
+                            {/* icons */}
+                            <img src="./live-preview-icon.png" alt="live preview icon" />
+                            <span className="text-sm underline">Live Preview</span>
+
+                            <img src="./github-icon.png" alt="githud icon" />
+                            <span className="text-sm underline">View Code</span>
+                        </div>
+                    </div>
+                </div>
+
+                {/* project 5 */}
+                <div className="max-w-[250px] pb-2 m-4 bg-white rounded-xl shadow">
+                    {/* image */}
+                    <img src="/images/project-5.png" alt="My fifth project" />
+
+                    <div className="m-3">
+                        <h3 className="font-medium text-center">Project Tile goes here</h3>
+                        {/* text content */}
+                        <p className="font-light">This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</p>
+
+                        <span className="text-sm m-3 inline-block">Tech stack : HTML , JavaScript, SASS, React</span>
+
+                        <div className="inline-flex flex gap-2">
+                            {/* icons */}
+                            <img src="./live-preview-icon.png" alt="live preview icon" />
+                            <span className="text-sm underline">Live Preview</span>
+
+                            <img src="./github-icon.png" alt="githud icon" />
+                            <span className="text-sm underline">View Code</span>
+                        </div>
+                    </div>
+                </div>
+
+                {/* project 6 */}
+                <div className="max-w-[250px] pb-2 m-4 bg-white rounded-xl shadow">
+                    {/* image */}
+                    <img src="/images/project-6.png" alt="My sixth project" />
+
+                    <div className="m-3">
+                        <h3 className="font-medium text-center">Project Tile goes here</h3>
+                        {/* text content */}
+                        <p className="font-light">This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</p>
+
+                        <span className="text-sm m-3 inline-block">Tech stack : HTML , JavaScript, SASS, React</span>
+
+                        <div className="inline-flex flex gap-2">
+                            {/* icons */}
+                            <img src="./live-preview-icon.png" alt="live preview icon" />
+                            <span className="text-sm underline">Live Preview</span>
+
+                            <img src="./github-icon.png" alt="githud icon" />
+                            <span className="text-sm underline">View Code</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
